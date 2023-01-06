1f99f0f44ebb4c6bbdb53e58a4b6440209071144
Rakshith <92984214+rakshithm01@users.noreply.github.com>
2023-01-06 12:34:10 +0530

    Update xarray.py

--- xarray.py
+++ xarray.py
@@ -1,9 +1,15 @@
 import xarray as xr
-ds = xr.open_dataset('specific_humidity.nc')
+import numpy as np
+import catopy.crs as ccrs
+ds = xr.open_dataset('pr_wtr.nc')
+ds #Will show all details
 
-print(ds.dims)
-print(ds.coords)
+ds1 = ds.sel(time='2022-01-15') #A particular data point
+ds2 = ds.sel(time=slice("2001-01-15", "2001-12-15"), lon=slice(70, 90), lat=slice(0, 30)) #All values of 2001, over 10-30N and 70-90E
 
-print(ds['time'].values)
 
-q = ds['q'].sel(time=f'{year}').values
+#Plotting 
+ax = plt.axes(projection=ccrs.PlateCarree())
+ax.coastlines()
+ax.gridlines(draw_labels=True)
+ds1.toa_net_all_mon.plot() #toa_net_all_mon is a variable
